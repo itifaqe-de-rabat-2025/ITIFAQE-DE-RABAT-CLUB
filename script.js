0a2e80d99746490cbb3a98d7f0931485326576b5
itifaqe.de.rabat.2025 <itifaqe.de.rabat.contact@gmail.com>
2026-07-29 15:13:40 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const menu = document.querySelector(".menu");
+
+menu.addEventListener("click", function () {
+    alert("قائمة النادي: الرئيسية - الإدارة - المدربين - اللاعبين - الأخبار - التواصل");
+});
